4a284a2fa255662761ad222741b26722221c8391
wmhst7 <wmhst@qq.com>
2019-09-22 16:57:43 +0800

    1

--- Jump.cpp
+++ Jump.cpp
@@ -3,90 +3,10 @@
 #include <cstring>
 using namespace std;
 #define INF 0x3f3f3f3f
-int N, M, K;
-char A[501010], B[501010];
-int res;
-bool flag; //true->d is the first
-int *d, *p, *a, *b;
-
-int mmin(int a, int b, int c = INF)
-{
-    int t = a < b ? a : b;
-    return t < c ? t : c;
-}
+#define ll long long
 
 int main()
 {
-    a = new int[501010];
-    b = new int[501010]; //优化存储
-    scanf("%d %d %d", &N, &M, &K);
-    scanf("%s", A);
-    scanf("%s", B);
-    flag = true;
-    d = a;
-    p = b;
-    for (int i = 0; i <= M; i++)
-    {
-        d[i] = i;
-    }
-    for (int i = 1; i <= N; i++)
-    {
-        int lo = 0;
-        int hi = M;
-        for (int j = lo; j <= hi; j++)
-        {
-            if (flag)
-            {
-                d = a;
-                p = b;
-            }
-            else
-            {
-                d = b;
-                p = a;
-            }
-            if (j == 0)
-            {
-                p[j] = i;
-                continue;
-            }
-            if (A[i - 1] == B[j - 1])
-            {
-                if (j == i - K - 1)
-                {
-                    p[j] = mmin(d[j] + 1, d[j - 1]);
-                }
-                else
-                {
-                    p[j] = mmin(p[j - 1] + 1, d[j] + 1, d[j - 1]);
-                }
-            }
-            else
-            {
-                if (j == i + K + 1)
-                {
-                    p[j] = mmin(p[j - 1], d[j - 1] + 2);
-                }
-                else
-                {
-                    p[j] = mmin(p[j - 1] + 1, d[j] + 1, d[j - 1] + 2);
-                }
-            }
-        }
-        flag = !flag;
-
-        // for (int k = 0; k <= M; k++)
-        // {
-        //     cout << "d[" << k << "]=" << d[k] << '\t';
-        // }
-        // cout << endl;
-        // for (int k = 0; k <= M; k++)
-        // {
-        //     cout << "p[" << k << "]=" << p[k] << '\t';
-        // }
-        // cout << endl;
-    }
-    res = p[M] > K ? -1 : p[M];
-    cout << res << endl;
+    
     return 0;
 }
