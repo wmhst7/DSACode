0b205571185230c4370522ce60eef622b3fd5734
wmhst7 <wmhst@qq.com>
2019-09-27 20:57:13 +0800

    4

--- Jump.cpp
+++ Jump.cpp
@@ -2,73 +2,91 @@
 #include <cstdlib>
 #include <cstring>
 using namespace std;
-#define INF 0x3f3f3f3f
 #define ll long long
 int N;
-ll l[1000010], r[1000010], t[1000010], T[1000010], elem[1000010];
+ll l[1000010], r[1000010];
+ll T[1000010], t[1000010];
+int buffer[1000010];
 
-struct queue
+template <typename T>
+class Queue
 {
-    int _head;
-    int _tail;
+private:
+    int _head, _tail;
+    int _capacity;
+    T *_elem;
 
-    void show()
-    {
-        // cout << "queue: ";
-        // for (int i = _head; i < _tail; i++)
-        // {
-        //     cout << elem[i] << ' ';
-        // }
-        // cout << endl;
-    }
-
-    queue()
+public:
+    Queue()
     {
         _head = 0;
         _tail = 0;
+        _capacity = 1000010;
+        _elem = (int *)buffer;
     }
-
-    void enqueue(int data)
-    {
-        elem[_tail] = data;
-        _tail++;
-        //cout<<"enqueue ";
-        show();
-    }
-
-    void dequeue()
-    {
-        if (_tail - _head > 0)
-            _head++;
-        //cout<<"dequeue ";
-        show();
-    }
-
-    void dequeue_t()
-    {
-        if (_tail - _head > 0)
-            _tail--;
-        //cout<<"dequeue_t ";
-        show();
-    }
-
-    ll head()
-    {
-        if(_tail - _head == 0)
-            return -1;
-        return elem[_head];
-    }
-
-    ll tail()
-    {
-        return elem[_tail - 1];
-    }
-
-    bool isempty()
-    {
-        return _head == _tail ? true : false;
-    }
+    bool empty();
+    bool push_back(const T);
+    bool pop_before();
+    bool pop_back();
+    T &front();
+    T &back();
+    void show();
 };
+
+template <typename T>
+bool Queue<T>::empty()
+{
+    return _head == _tail ? true : false;
+}
+
+template <typename T>
+bool Queue<T>::push_back(const T t)
+{
+    if (_tail > _capacity - 1)
+        return false;
+    _elem[_tail] = t;
+    _tail++;
+    return true;
+}
+
+template <typename T>
+bool Queue<T>::pop_before()
+{
+    if (_tail - _head <= 0)
+        return false;
+    _head++;
+    return true;
+}
+
+template <typename T>
+bool Queue<T>::pop_back()
+{
+    if (_tail - _head <= 0)
+        return false;
+    _tail--;
+    return true;
+}
+
+template <typename T>
+T &Queue<T>::front()
+{
+    return _elem[_head];
+}
+
+template <typename T>
+T &Queue<T>::back()
+{
+    return _elem[_tail - 1];
+}
+
+template <typename T>
+void Queue<T>::show()
+{
+    cout<<"Queue: ";
+    for(int i = _head;i<_tail;i++){
+        cout<<_elem[i]<<' ';
+    }cout<<endl;
+}
 
 int main()
 {
@@ -77,39 +95,37 @@ int main()
     {
         scanf("%lld %lld %lld", &l[i], &r[i], &t[i]);
     }
-    queue q;
+    Queue<int> q;
     //先处理i = N, N-1
     T[N] = 0;
-    T[N-1] = t[N-1];
-    q.enqueue(N);
+    T[N - 1] = t[N - 1];
+    q.push_back(N);
     for (int i = N - 2; i >= 1; i--)
     {
-        for (int j = l[i+1] - 1; j >= l[i]; j--)
+        for (int j = l[i + 1]; j >= l[i]; j--)
         {
-            // cout<<"i = "<<i<<" j = "<<j<<endl;
-            // cout<<"l[i] = "<<l[i]<<" r[i] = "<<r[i]<<endl;
-            while (!q.isempty() && T[q.tail()] >= T[j])
+            while (!q.empty() && T[j] <= T[q.back()])
             {
-                q.dequeue_t();
+                q.pop_back();
             }
-            q.enqueue(j);
-            while (!q.isempty() && q.head() > r[i])
+            q.push_back(j);
+            while (!q.empty() && q.front() > r[i])
             {
-                q.dequeue();
+                q.pop_before();
             }
         }
-        if (q.head() == -1)
+        //q.show();
+        if (q.empty())
         {
-            T[i] = INF;
+            T[i] = -1;
         }
         else
         {
-            T[i] = T[q.head()] + t[i];
+            T[i] = t[i] + T[q.front()];
         }
-        //cout << "T[" << i << "] = " << T[i] << ", q.head = " << q.head() << endl;
     }
-    cout<<T[1]<<endl;    
+    //for (int i = 1; i <= N; i++)
+        //cout << "T[" << i << "]: " << T[i] << endl;
+    printf("%lld\n", T[1]);
     return 0;
 }
-
-
--- MyJump2.cpp
+++ MyJump2.cpp
@@ -2,13 +2,16 @@
 #include <cstdlib>
 #include <cstring>
 using namespace std;
-#define INF 0x3f3f3f3f
 #define ll long long
 int N;
 ll l[1000010], r[1000010], t[1000010], T[1000010];
 
 ll mmin(ll a, ll b)
 {
+    if (a == -1)
+        return b;
+    if (b == -1)
+        return a;
     return a < b ? a : b;
 }
 
@@ -20,15 +23,18 @@ int main()
         scanf("%lld %lld %lld", &l[i], &r[i], &t[i]);
     }
     T[N] = 0;
-    for(int i = N-1;i>=1;i--){
-        ll tem = INF;
-        for(int j = l[i];j<=r[i];j++){
+    T[N - 1] = t[N - 1];
+    for (int i = N - 2; i >= 1; i--)
+    {
+        ll tem = -1;
+        for (int j = l[i]; j <= r[i]; j++)
+        {
             tem = mmin(tem, T[j]);
         }
-        T[i] = t[i]+tem;
+        T[i] = t[i] + tem;
     }
-    // for (int i = 1; i <= N; i++)
-    //     cout << "T[" << i << "]: " << T[i] << endl;
+    for (int i = 1; i <= N; i++)
+        cout << "T[" << i << "]: " << T[i] << endl;
     printf("%lld\n", T[1]);
     return 0;
 }
