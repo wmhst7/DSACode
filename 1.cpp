35861d6b8ac74d22705ec35b79d46445689b6fd4
wmhst7 <wmhst@qq.com>
2019-10-09 21:29:28 +0800

    5

--- 1.cpp
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    double a = 25.0 - 1e-8;
-    double b = 200.0;
-    printf("%lf %lf %lf\n", a, b, a * b);
-}
--- b.py
+++
@@ -1,6 +0,0 @@
-n = int(input())
-for itr in range(n):
-	a, b = input().split(" ")
-	a = int(a)
-	b = int(b)
-	print(a * b)
